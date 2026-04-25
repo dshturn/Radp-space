54fbbfc7050f60d525d11e25db04c47063c44a6f
dshtu <dshturn@gmail.com>
2026-04-25 19:10:49 +0300

    Auto-save from Claude session

--- js/equipment.js
+++ js/equipment.js
@@ -378,7 +378,7 @@ function equipItemCard(item, name, docs, subs = [], docsByItem = {}, subsByParen
         <div class="card-title">${safeName}</div>
         <div class="card-meta">
           <div class="doc-name">S/N: ${safeSerial}</div>
-          ${isAdmin && item.user_profiles?.company ? `<div class="doc-name" style="color:var(--text-3);font-size:11px;">${esc(item.user_profiles.company)}</div>` : ''}
+          ${isAdmin && item.contractor_id?.company ? `<div class="doc-name" style="color:var(--text-3);font-size:11px;">${esc(item.contractor_id.company)}</div>` : ''}
           ${alertBadge}
         </div>
       </div>
