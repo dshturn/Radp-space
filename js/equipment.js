736c13d42083857ee7595edcdf033d97925b11e1
dshtu <dshturn@gmail.com>
2026-04-26 10:44:57 +0300

    Auto-save from Claude session

--- js/equipment.js
+++ js/equipment.js
@@ -328,7 +328,7 @@ function equipItemCard(item, name, docs, subs = [], docsByItem = {}, subsByParen
               </div>
               <div class="doc-group-body"><div class="doc-group-inner">
                 ${scDocsHtml}
-                <button class="upload-btn upload-btn-sm" onclick="openAddDoc(${parseInt(sc.id)}, '${sc.name.replace(/'/g, "\\'")}')">+ Add Document</button>
+                <button class="upload-btn upload-btn-sm" onclick="openAddDoc(${parseInt(sc.id)}, this.dataset.name)" data-name="${esc(sc.name)}">+ Add Document</button>
               </div></div>
             </div>
           </div></div>
