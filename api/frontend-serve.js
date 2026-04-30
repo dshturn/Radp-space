84177ce37758097363378d7b3f8fa989151deda7
dshtu <dshturn@gmail.com>
2026-04-30 14:10:49 +0300

    Auto-save from Claude session

---
+++ api/frontend-serve.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicDir = path.join(__dirname, '../public');
+const jsDir = path.join(__dirname, '../js');
+
+console.log('Serving public files from:', publicDir);
+console.log('Serving JS files from:', jsDir);
+
+app.use(express.static(publicDir, { index: false }));
+app.use('/js', express.static(jsDir));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicDir, 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('Not found');
+});
+
+app.listen(3000, () => {
+  console.log('Frontend running on http://localhost:3000');
+});
