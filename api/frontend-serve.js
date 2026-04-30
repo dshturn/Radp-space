67756f91a6c25e830100f4b47879bb1a1a735d1a
dshtu <dshturn@gmail.com>
2026-04-30 14:11:59 +0300

    Auto-save from Claude session

--- api/frontend-serve.js
+++ api/frontend-serve.js
@@ -19,6 +19,6 @@ app.get('*', (req, res) => {
   res.status(404).send('Not found');
 });
 
-app.listen(3000, () => {
-  console.log('Frontend running on http://localhost:3000');
+app.listen(3001, () => {
+  console.log('Frontend running on http://localhost:3001');
 });
