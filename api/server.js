f0ef21251bd80d966334a333815bdeb42505d339
dshtu <dshturn@gmail.com>
2026-05-02 20:29:21 +0300

    Auto-save from Claude session

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const pdf = require('html-pdf');
+const puppeteer = require('puppeteer');
 const { PDFDocument } = require('pdf-lib');
 require('dotenv').config();
 
